d455335e302db14864846e95dc4e1e24020cf580
jankubierecki <jankubierecki@gmail.com>
2018-09-26 21:02:23 +0200

    home page view unit test generates html

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,29 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 
 class HomePageTestCase(TestCase):
 
     def test_root_url_resolves_home_page(self):
+        """ tests url  main page routing """
+
+        # When
         found = resolve('/')
+
+        # Then
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+
+        # Given
+        request = HttpRequest()
+
+        # When
+        response = home_page(request)
+
+        # Then
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>TODO Lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def home_page():
-    pass
+def home_page(request):
+    """ main page view """ 
+    
+    return HttpResponse('<html><title>TODO Lists</title></html>')
