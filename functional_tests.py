e48704ed2cec39cdf7e31ff73e5365a3408a4849
jankubierecki <jankubierecki@gmail.com>
2018-09-27 20:37:31 +0200

    functional test now checks if user can add something to list

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -16,17 +17,35 @@ class NewVisitorTest(unittest.TestCase):
         # user wants to play with todoapp so he is heading to main page
         self.browser.get('http://localhost:8000')
 
-        # user has noticed that main page title has word 'Lists' in its header
+        # user has noticed that main page and header title has word 'Lists' in its header
         self.assertIn('Lists', self.browser.title)
-        self.fail('Test has ended')
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+
+        self.assertIn('Lists', header_text)
+
+        # user noticed text area field with placeholder
+        input_box = self.browser.find_element_by_id('id_new_item')
+
+        self.assertEqual(input_box.get_attribute(
+            'placeholder'), 'Enter your task!')
 
         # in the text area user has put 'buy Peacock feathers' ( its his Hobby )
+        input_box.send_keys('buy Peacock feathers')
 
         # after user pressed enter button, the list got updated and now it contains the above user's thing to do
+        input_box.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+
+        self.assertTrue(
+            any(row.text == '1. buy Peacock feathers' for row in rows)
+        )
 
         # on the main page there is still text area field that can accept another user's thing to do
-
         # the user wrote 'use peacock feathers as a component of a bait recipie' as a next thing do to in the text area
+        self.fail('Test has ended')
 
         # the page has been updated again, and now it displays two user's works
 
