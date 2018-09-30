08cf1930d48ed919a81acb3df09c06e684e3e736
jankubierecki <jankubierecki@gmail.com>
2018-09-30 19:20:16 +0200

    Item model

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
-from lists.views import home_page
+from .views import home_page
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+
+from .models import Item
 
 from django.template.loader import render_to_string
 
@@ -47,3 +49,26 @@ class HomePageTestCase(TestCase):
         # Then
         self.assertIn('New list element', response.content.decode())
         self.assertEqual(response.content.decode(), expected_html)
+
+
+class ItemModelTestCase(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+
+        # Given
+        first_item = Item()
+        second_item = Item()
+        first_item.text = 'First item in list'
+        second_item.text = 'Second item in list'
+
+        # When
+        first_item.save()
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        first_saved_item, second_saved_item = saved_items[0], saved_items[1]
+
+        # Then
+        self.assertEqual(saved_items.count(), 2)
+        self.assertEqual(first_saved_item.text, 'First item in list')
+        self.assertEqual(second_saved_item.text, 'Second item in list')
