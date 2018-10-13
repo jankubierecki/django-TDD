b5bda46c0207845948dbebdefbae50382bbbecca
jankubierecki <jankubierecki@gmail.com>
2018-10-13 23:17:11 +0200

    view for adding items to existing list

--- lists/tests.py
+++ lists/tests.py
@@ -73,24 +73,53 @@ class ListAndItemModelsTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
-
-        self.assertTemplateUsed(response, 'list.html')
-
-    def test_displays_all_items(self):
 
         # Given
         list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        # When
+        response = self.client.get('/lists/%d/' % (list_.id,))
 
+        # Then
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_items_for_particular_list(self):
+
+        # Given
+        list_correct = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_correct)
+        Item.objects.create(text='itemey 2', list=list_correct)
+
+        list_other = List.objects.create()
+        Item.objects.create(text='itemey 1 of other list', list=list_other)
+        Item.objects.create(text='itemey 2 of other list', list=list_other)
+
+        # When
+        response = self.client.get('/lists/%d/' % (list_correct.id,))
+
+        # Then
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
 
+        self.assertNotContains(response, 'itemey 1 of other list')
+        self.assertNotContains(response, 'itemey 2 of other list')
+
+    def test_passes_correct_list_to_template(self):
+
+        # Given
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        # When
+        response = self.client.post(
+            '/lists/%d/' % (correct_list.id,))
+
+        # Then
+        self.assertEqual(response.context['list'], correct_list)
+
 
 class NewListTest(TestCase):
+
     def test_saving_POST_request(self):
 
         # When
@@ -110,6 +139,40 @@ class NewListTest(TestCase):
             data={'new_item': 'New list element'}
         )
 
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'],
-                         '/lists/the-only-list-in-the-world/')
+        list_new = List.objects.first()
+        self.assertRedirects(response, '/lists/%d/' % (list_new.id,))
+
+
+class NewItemInListTest(TestCase):
+
+    def test_can_save_POST_request_to_existing_list(self):
+
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        # When
+        self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'new_item': 'New item for an existing list'}
+        )
+
+        new_item = Item.objects.first()
+
+        # Then
+        self.assertEqual(Item.objects.count(), 1)
+
+        self.assertEqual(new_item.text, 'New item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        # When
+        response = self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'new_item': 'New item for an existing list'}
+        )
+
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
--- superlists/urls.py
+++ superlists/urls.py
@@ -21,7 +21,9 @@ from lists import views as list_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', list_views.home_page, name='home'),
-    url(r'^lists/the-only-list-in-the-world/$',
-        list_views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/$', list_views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/add_item$',
+        list_views.add_item_to_existing_list, name='add_item'),
     url(r'^lists/new$', list_views.new_list, name='new_list'),
+
 ]
