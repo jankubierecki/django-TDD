73d5f63ea10dc91163d2f6159e7946a0fa6ebd2c
jankubierecki <jankubierecki@gmail.com>
2018-10-19 20:17:05 +0200

    changes functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,8 +2,24 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+import sys
+
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(self):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                self.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        self.server_url = self.live_server_url
+
+    @classmethod
+    def tearDownClass(self):
+        if self.server_url == self.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -21,7 +37,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_list_and_retieve_later(self):
 
         # user wants to play with todoapp so he is heading to main page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # user has noticed that main page and header title has word 'Lists'
         # in its header
@@ -67,7 +83,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # another user visits main page
         # he does not see any possible traces left by first user
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         page_text = self.browser.find_element_by_tag_name('body').text
 
@@ -95,7 +111,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_layout_and_styling(self):
 
         # user goes to main page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # user notices that the text field is centered
